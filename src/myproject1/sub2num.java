9b4ebea35223572b6cc9cf6e37c28fe0f73a3ec8
Alavalapati <prashanthreddy.alavalapati@sky.uk>
2024-02-15 15:13:33 +0530

    secondtime

---
+++ src/myproject1/sub2num.java
@@ -0,0 +1,10 @@
+package myproject1;
+
+public class sub2num {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a-b;
+        System.out.println(c);
+    }
+}
