908f2fe18249cb5dd9bb174b7a5b6176ffc58501
Alavalapati <prashanthreddy.alavalapati@sky.uk>
2024-02-15 15:08:49 +0530

    firsttime

---
+++ src/myproject2/add3num.java
@@ -0,0 +1,11 @@
+package myproject2;
+
+public class add3num {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int d = 30;
+        int c = a+b+d;
+        System.out.println(c);
+    }
+}
